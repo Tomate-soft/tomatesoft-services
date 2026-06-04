4bba1b9848463841b5d57abf565821c8fa0fdea1
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-04 11:41:16 -0600

    Update AppService to return a localized greeting message

--- apps/tomatesoft-services/src/app.service.ts
+++ apps/tomatesoft-services/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Hola yo soy el servicio de tomatesoft-services';
   }
 }
