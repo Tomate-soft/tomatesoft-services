a105999a3b11b82fcc201d88b3d7d84cce489a1f
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-04 11:48:32 -0600

    Update AppService to include project context in greeting message

--- apps/tomatesoft-services/src/app.service.ts
+++ apps/tomatesoft-services/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Hola yo soy el servicio de tomatesoft-services';
+    return 'Hello World! Hola yo soy el servicio de tomatesoft-services he estamos trabajando en el proyecto de microservicios con NestJS';
   }
 }
