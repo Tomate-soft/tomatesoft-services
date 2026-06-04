fe076052ea2bc2eb2fba0c5bbe6a65a9aa4c1c7a
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-04 11:52:38 -0600

    Update AppService to include additional context in greeting message

--- apps/tomatesoft-services/src/app.service.ts
+++ apps/tomatesoft-services/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Hola yo soy el servicio de tomatesoft-services he estamos trabajando en el proyecto de microservicios con NestJS';
+    return 'Hello World! Hola yo soy el servicio de tomatesoft-services he estamos trabajando en el proyecto de microservicios con NestJS y se supóne que ahora no deberai hacer redseploy de el otro microservicio que es el taunter-service pero si hacemos cambios en este servicio si se tiene que hacer redeploy de el otro servicio para que pueda consumir los cambios que se hicieron en este servicio';
   }
 }
