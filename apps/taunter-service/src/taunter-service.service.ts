545cd4c0afff733e759448b778f6ae877c1b5fa8
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-04 13:24:49 -0600

    Refactor TaunterService and AppService to simplify greeting messages

--- apps/taunter-service/src/taunter-service.service.ts
+++ apps/taunter-service/src/taunter-service.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class TaunterServiceService {
   getHello(): string {
 
-    return 'Hello World! Hola yo soy el servicio de taunter-service, sin embargo ahora si deberairedeployarse y por el momento se hara de´ploy en lños dos, pero se supone que si hacemos cambios en el servicio de tomatesoft-services no se tiene que hacer redeploy del servicio de taunter-service para que pueda consumir los cambios que se hicieron en el servicio de tomatesoft-services y uno mas por que no. , com ya esta corregidom ahora si deberia de diferenciarse y hacer de plaoy enc ad auno con normalidad';
+    return 'Listo taunter service';
   }
 }
