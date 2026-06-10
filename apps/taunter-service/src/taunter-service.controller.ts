fbb0a2d5b20b4fc9b6805c123e08f21626a5a7e4
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-10 17:46:15 -0600

    add transport

--- apps/taunter-service/src/taunter-service.controller.ts
+++ apps/taunter-service/src/taunter-service.controller.ts
@@ -1,10 +1,10 @@
 import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { EventPattern, Payload, Transport } from '@nestjs/microservices';
 import { TAUNTER_REQUEST_EVENT } from '../shared/queue.tokens';
 
 @Controller()
 export class TaunterServiceController {
-  @EventPattern(TAUNTER_REQUEST_EVENT)
+  @EventPattern(TAUNTER_REQUEST_EVENT, Transport.RMQ)
   handleTaunterRequest(@Payload() message: unknown) {
     console.log('TAUNTER_REQUEST_EVENT received:', message);
   }
