6aac4f783da2b0c4f032dac5525131a0d4426590
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-12 17:20:22 -0600

    recovery config

--- apps/recovery-event-service/src/main.ts
+++ apps/recovery-event-service/src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { RecoveryEventServiceModule } from './recovery-event-service.module';
+import { RabbitmqQueueModule } from '@app/shared/rabbitmq-queue/rabbitmq-queue.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RecoveryEventServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const options = {
+    credentials: {
+      host: process.env.RABBITMQ_HOST || 'fallback_host',
+      password: process.env.RABBITMQ_PASSWORD || 'fallback_password',
+      port: Number(process.env.RABBITMQ_PORT) || 666,
+      vhost: process.env.RABBITMQ_VHOST || '/',
+      user: process.env.RABBITMQ_USER || 'fallback_user',
+    },
+    queue: {
+      name: process.env.RABBITMQ_QUEUE_NAME || 'fallback_queue',
+      deadLetter: {
+        exchange: 'dlx',
+        patterns: ['TAUNTER_REQUEST_EVENT'],
+      },
+    },
+  };
+  const microService =
+    await RabbitmqQueueModule.createRecoveryMicroserviceOptions(options);
+
+  const app = await NestFactory.createMicroservice(
+    RecoveryEventServiceModule,
+    microService,
+  );
+  await app.listen();
 }
 bootstrap();
-// -- bui---
