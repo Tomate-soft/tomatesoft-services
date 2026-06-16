1c59abd05accbfe3ae31d1969dcaf7a4270a80cc
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-16 15:24:06 -0600

    asdf

--- apps/api-gateway/src/modules/taunter/taunter.module.ts
+++ apps/api-gateway/src/modules/taunter/taunter.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { WriteTaunterController } from './controllers/write-taunter.controller';
 import { WriteTaunterService } from './services/write-taunter.service';
+import { TAUNTER_REQUEST_EVENT } from '../../common/prefixes';
+import { RabbitmqQueueModule } from '@app/shared/rabbitmq-queue/rabbitmq-queue.module';
+
+const options = {
+  credentials: {
+    host: process.env.RABBITMQ_HOST || 'fallback_host',
+    password: process.env.RABBITMQ_PASSWORD || 'fallback_password',
+    port: Number(process.env.RABBITMQ_PORT) || 666,
+    vhost: process.env.RABBITMQ_VHOST || '/',
+    user: process.env.RABBITMQ_USER || 'fallback_user',
+  },
+  queue: {
+    name: process.env.RABBITMQ_QUEUE_NAME || 'fallback_queue',
+    deadLetter: {
+      exchange: 'dlx',
+      patterns: [TAUNTER_REQUEST_EVENT],
+    },
+  },
+};
 
 @Module({
+  imports: [RabbitmqQueueModule.register(options)],
   controllers: [WriteTaunterController],
   providers: [WriteTaunterService],
 })
