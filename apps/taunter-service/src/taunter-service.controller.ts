4075800e1595900af6ad4936c6cffcad8286f327
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-16 15:33:53 -0600

    sdf

--- apps/taunter-service/src/taunter-service.controller.ts
+++ apps/taunter-service/src/taunter-service.controller.ts
@@ -12,10 +12,10 @@ import { TAUNTER_REQUEST_EVENT } from '../shared/queue.tokens';
 export class TaunterServiceController {
   @EventPattern(TAUNTER_REQUEST_EVENT, Transport.RMQ)
   handleTaunterRequest(
-    @Payload() message: unknown,
+    @Payload() data: any,
     @Ctx() context: RmqContext,
   ) {
-    console.log('TAUNTER_REQUEST_EVENT received:', message);
+    console.log('TAUNTER_REQUEST_EVENT received:', data);
     // CASO ÉXITO: Le confirmamos a RabbitMQ para que borre el mensaje de la cola
     // context.getChannelRef().ack(context.getMessage());
     context.getChannelRef().reject(context.getMessage(), false);
