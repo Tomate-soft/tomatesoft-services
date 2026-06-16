6f1d08e2035622e9819f226b86dd636a8e969414
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-06-16 11:35:05 -0600

    test gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { GLOBAL_PREFIX } from './common/prefixes';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+  app.setGlobalPrefix(GLOBAL_PREFIX);
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
